174ca002a443b419c319f5717f42c58c1ff3e88a
Tim Heaton <tim.heaton@utah.edu>
2015-05-20 22:03:46 +0000

    Add config.ru

--- site.rb
+++ site.rb
@@ -1,16 +1,17 @@
 require 'sinatra'
+
 ENV['PORT'] ||= '4000'
 set :port, ENV['PORT']
 set :bind, '0.0.0.0'
+
 get '/' do
-"I'm alive!"
+  erb :index
 end
+
 get '/italian' do
     "Ciao!"
 end
 get '/german' do
   "Guten Tag Liebchen!"
 end
-get '/' do
-  erb :index
-end
+
